9fe22be43a3c63aff04f73d9b7a020345c764140
Lukas <lukas_weymann@hotmail.com>
2020-06-22 17:28:17 +0200

    Added the two new required routing functionalities to show a chosen pokemon from the list

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -7,4 +7,26 @@ router.get("/", function (req, res, next) {
   res.send(pokedex);
 });
 
+router.get("/:id/:info", function (req, res, next) {
+  const { id, info } = req.params;
+  const idAsNumber = parseInt(id, 10);
+
+  const foundPokemon = pokedex.find((singlePokemon) => {
+    return singlePokemon.id === idAsNumber;
+  });
+
+  res.send(foundPokemon[info]);
+});
+
+router.get("/:id", function (req, res, next) {
+  const { id } = req.params;
+  const idAsNumber = parseInt(id, 10);
+
+  const foundPokemon = pokedex.find((singlePokemon) => {
+    return singlePokemon.id === idAsNumber;
+  });
+
+  res.send(foundPokemon);
+});
+
 module.exports = router;
